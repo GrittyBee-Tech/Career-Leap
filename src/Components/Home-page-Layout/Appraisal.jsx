6010090efef6498704b5f6c1a5fbf2d8cc09fc8d
fiiwebarry <fiiwebarry@yahoo.com>
2023-10-27 14:34:01 +0100

    update

--- src/Components/Home-page-Layout/Appraisal.jsx
+++ src/Components/Home-page-Layout/Appraisal.jsx
@@ -10,29 +10,29 @@ const Appraisal = () => {
                     <img className="w-[600px] pt-11" src={Icon} alt="icon" />
 
                 </div>
-                <div className="mt-6">
+                <div className="md:mt-6 mt-3">
                     <h2 className="text-[#00F] text-[40px] md:text-[60px] font-plus-jakarta-sans font-semibold">
                         Appraisals
                     </h2>
-                    <p className="text-[#0A0A29] md:text-[30px] text-[20px] font-medium font-plus-jakarta-sans mt-[30px]"> Interactive Employee Appraisals</p>
+                    <p className="text-[#0A0A29] md:text-[30px] text-[20px] font-medium font-plus-jakarta-sans md:mt-[30px]  mt-6"> Interactive Employee Appraisals</p>
 
-                    <div className="flex gap-3 mt-[30px]">
+                    <div className="flex gap-3 md:mt-[30px] mt-8">
                         <img className="w-[45px] h-[45px]" src={check} alt="check" />
                         <p className="w-[446px] h-[60px] text-[#0A0A29] md:text-[24px] font-normal font-plus-jakarta-sans"> Set goals for your employees and track their progress over time.</p>
                     </div>
-                    <div className="flex gap-3  md:mt-7">
+                    <div className="flex gap-3  md:mt-7  mt-5 ">
                         <img className="w-[45px] h-[45px]" src={check} alt="check" />
                         <p className="w-[446px] h-[60px] text-[#0A0A29] md:text-[24px] font-normal font-plus-jakarta-sans">Determine employee salary and benefits.</p>
                     </div>
-                    <div className="flex gap-3 md:mt-7">
+                    <div className="flex gap-3 md:mt-7 mt-1">
                         <img className="w-[45px] h-[45px]" src={check} alt="check" />
                         <p className="w-[446px] h-[60px] text-[#0A0A29] md:text-[24px] font-normal font-plus-jakarta-sans">Set goals for your employees and track their progress over time.</p>
                     </div>
-                    <div className="flex gap-3 md:mt-7">
+                    <div className="flex gap-3 md:mt-7 mt-3">
                         <img className="w-[45px] h-[45px]" src={check} alt="check" />
                         <p className="w-[446px] h-[60px] text-[#0A0A29] md:text-[24px] font-normal font-plus-jakarta-sans">Identify employees who are ready for promotions or transfers.</p>
                     </div>
-                    <div className="flex gap-3 md:mt-7 mb-11">
+                    <div className="flex gap-3 md:mt-7 mb-11 mt-3">
                         <img className="w-[45px] h-[45px]" src={check} alt="check" />
                         <p className="w-[446px] h-[60px] text-[#0A0A29] md:text-[24px] font-normal font-plus-jakarta-sans leading-7">Track employee progress over time to identify areas where employees may need additional support or training.</p>
                     </div>
