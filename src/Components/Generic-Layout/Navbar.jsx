01bf4cc6cd7afe9b5da8b4f5ff0ff3d604280bb2
fiiwebarry <fiiwebarry@yahoo.com>
2023-10-31 18:32:13 +0100

    update on tablet-view

--- src/Components/Generic-Layout/Navbar.jsx
+++ src/Components/Generic-Layout/Navbar.jsx
@@ -15,17 +15,17 @@ const Navbar = () => {
 
     return (
         <nav>
-            <div className="container m-auto md:w-[85%] w-[94%] flex items-center justify-between md:gap-[80px] mt-4 md:border-none  border border-[#00F] rounded md:p-2 p-3">
+            <div className="container m-auto md:w-[94%] w-[94%]  lg:w-[85%] flex items-center justify-between lg:gap-[80px] md:gap-[20px] mt-4  md:p-2 p-3">
                 <div className="flex items-center">
-                    <span className="text-[32px]  text-[#00F] font-normal  font-lobster ">
+                    <span className=" md:text-[25px] text-[35px] md:mb-4 lg:text-[32px] text-[#00F] font-normal  font-lobster ">
                         RisePath
                     </span>
-                    <span className="text-[36px]  text-[#00F]">
+                    <span className="lg:text-[36px] md:text-[25px]  text-[35px] md:mb-5 text-[#00F]">
                         <AiOutlineDingtalk />
                     </span>
                 </div>
 
-                <ul className="md:flex hidden gap-5 mt-2 text-[24px] text-[#0A0A29] font-plus-jakarta-sans font-semibold">
+                <ul className="md:flex hidden  md:mb-6 lg:mb-0 lg:gap-5 md:gap-2 mt-2 text-[17px] lg:text-[24px] text-[#0A0A29] font-plus-jakarta-sans font-semibold">
                     <li>
                         <Link to="/">
                             Home</Link></li>
@@ -37,11 +37,11 @@ const Navbar = () => {
                     <li>Mentorship</li>
                     <li>Mental Health</li>
                 </ul>
-                <div className=" md:flex gap-4 hidden">
-                    <p className="mt-2 text-[24px] text-[#0A0A29] font-plus-jakarta-sans font-semibold ">Log in</p>
-                    <button onClick={() => setIsModalOpen(true)} className="bg-[#00F]  text-[#FFFF]  w-[138px] h-[50px] rounded  text-[24px] font-bold">Sign up</button>
+                <div className=" md:flex lg:gap-4  md:gap-4 hidden lg:mb-0 mb-2">
+                    <p className="lg:mt-2 lg:mb-0  lg:text-[24px] md:text-[17px] text-[#0A0A29] font-plus-jakarta-sans font-semibold ">Log in</p>
+                    <button onClick={() => setIsModalOpen(true)} className="bg-[#00F]  text-[#FFFF]  lg:mb-0 mb-1  md:w-[90px] lg:w-[138px] lg:h-[50px] md:h-[35px] rounded md:text-[18px] lg:text-[24px] font-bold">Sign up</button>
                 </div>
-                <button onClick={toggleMenu} className="md:hidden flex ml-[84px] mt-4 text-[24px] text-[#00F]">
+                <button onClick={toggleMenu} className="md:hidden flex ml-[84px]  text-[24px] text-[#00F]">
                     <GiHamburgerMenu />
                 </button>
 
--- src/Components/Home-page-Layout/Hero.jsx
+++ src/Components/Home-page-Layout/Hero.jsx
@@ -7,9 +7,9 @@ const Hero = () => {
     return (
         <section id="Hero">
             <img className="absolute -z-20 md:w-[420px]  w-[150px] top-0 md:top-0 md:flex" src={icon} alt="icon" />
-            <div className="md:flex  container mx-auto w-[85%] mt-5 gap-[120px]">
+            <div className="md:flex  container mx-auto w-[85%] mt-5 lg:gap-[120px] md:gap-[5px]">
                 <div>
-                    <p className=" md:text-[45px]  text-[25px]  md:w-[515px] mt-[70px] font-normal  font-georgia">Build a more engaged and productive workforce with <span className="font-lobster text-[#00F]"> RisePath</span></p>
+                    <p className=" lg:text-[45px] md:text-[25px]  text-[25px]  lg:w-[515px] md:w-[412px] mt-[70px] font-normal  font-georgia">Build a more engaged and productive workforce with <span className="font-lobster text-[#00F]"> RisePath</span></p>
                     <p className="md:text-[24px]  text-[18px]  md:w-[486px] h-[60px]  mt-8 font-plus-jakarta-sans ">Give your employees a voice and help them to grow.</p>
 
                     <button className=" mt-7   w-[198px] h-[60px] bg-[#00F] text-[#FFFF]  font-bold text-[24px] rounded">Get Started</button>
