a70151d7ea509d5f6d1f86a0257c62c0e68fb7f3
izima01 <kingsleyizima@gmail.com>
2023-11-03 08:58:12 +0100

    edited the register admin page and the modal

--- src/Components/Generic-Layout/RegisterModal.jsx
+++ src/Components/Generic-Layout/RegisterModal.jsx
@@ -5,21 +5,23 @@ import { Link } from 'react-router-dom';
 
 const RegisterModal = ({ setIsModalOpen, isModalOpen }) => {
   return (
-    <section className={` md:absolute md:top-[120px] md:w-[70%]   md:rounded  w-full  h-auto fixed md:container mx-auto justify-center items-center bg-[#EAF6FC] registerOptions  md:h-[500px] inset-0 transition duration-500 ease-in-out ${isModalOpen ? 'flex' : 'hidden'}`}>
-      <button onClick={() => setIsModalOpen(false)} className='absolute top-7 right-9'><AiFillCloseCircle className="text-[#0000FF]" size={30} stroke='#0000FF' strk fill='#0000FF' /></button>
-      <div className="flex w-10/12 sm:flex-row flex-col gap-8 md:gap-16 justify-center items-center">
-        <div className="bg-white w-[22rem] px-6 pt-4 sm:pt-5 sm:pb-11 pb-6 flex flex-col items-center text-center gap-3 sm:gap-5 rounded-3xl regBox border border-[#407bff33]">
+    <section className={`fixed top-0 left-0 w-full justify-center items-center bg-[rgba(0,0,0,0.2)] inset-0 transition duration-500 ease-in-out ${isModalOpen ? 'flex' : 'hidden'}`}>
+      <div className="flex w-10/12 py-16 md:py-12 sm:flex-row flex-col gap-6 md:gap-12 justify-center items-center bg-[#EAF6FC] rounded-2xl relative">
+        <button onClick={() => setIsModalOpen(false)} className='absolute top-6 right-9'>
+          <AiFillCloseCircle className="text-[#0000FF]" size={30} />
+        </button>
+        <div className="bg-white w-[22rem] px-6 pt-3 sm:pt-5 sm:pb-11 pb-5 flex flex-col items-center text-center gap-3 sm:gap-5 rounded-3xl regBox border border-[#407bff33]">
           <img src={employee} alt="" />
-          <h3 className='text-[#0000FF] font-bold text-4xl sm:mb-2 font-plus-jakarta-sans'>Employee</h3>
+          <h3 className='text-[#0000FF] font-bold text-3xl sm:mb-2 font-plus-jakarta-sans'>Employee</h3>
           <p className='text-[#0000FF] font-semibold sm:mb-5 font-plus-jakarta-sans'>I am an employee i would like to sign in to my company dashboard</p>
-          <Link to='/employee/register' className='bg-[#3333FF] text-[#F0F0FF] rounded-lg px-5 py-2.5'>Register</Link>
+          <Link to='/employee/register' className='bg-[#3333FF] text-[#F0F0FF] rounded-lg px-5 py-2'>Register</Link>
         </div>
 
-        <div className="bg-white w-[22rem] px-6 pt-4 sm:pt-5 sm:pb-11 pb-6 flex flex-col items-center text-center gap-3 sm:gap-5 rounded-3xl regBox border border-[#407bff33]">
+        <div className="bg-white w-[22rem] px-6 pt-3 sm:pt-5 sm:pb-11 pb-5 flex flex-col items-center text-center gap-3 sm:gap-5 rounded-3xl regBox border border-[#407bff33]">
           <img src={employer} alt="" />
-          <h3 className='text-[#0000FF] font-bold text-4xl sm:mb-2 font-plus-jakarta-sans'>HR/CEO</h3>
+          <h3 className='text-[#0000FF] font-bold text-3xl sm:mb-2 font-plus-jakarta-sans'>HR/CEO</h3>
           <p className='text-[#0000FF] font-semibold sm:mb-5 font-plus-jakarta-sans'>I am a HR/CEO i would like to register as a company</p>
-          <Link to='/admin/register' className='bg-[#3333FF] text-[#F0F0FF] rounded-lg px-5 py-2.5'>Register</Link>
+          <Link to='/admin/register' className='bg-[#3333FF] text-[#F0F0FF] rounded-lg px-5 py-2'>Register</Link>
         </div>
       </div>
     </section>
--- src/Pages/Register/RegisterAdmin.jsx
+++ src/Pages/Register/RegisterAdmin.jsx
@@ -1,10 +1,11 @@
-import bg from '../../assets/Images/reg-Bg.svg';
+import bg from '../../assets/Images/amico.svg';
 import { useForm } from 'react-hook-form';
 import { AiOutlineMail, AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { BsArrowLeft } from 'react-icons/bs';
 
 const RegisterAdmin = () => {
   const [showPassword, setshowPassword] = useState(false);
@@ -25,25 +26,26 @@ const RegisterAdmin = () => {
   };
 
   return (
-    <section className=" bg-[#FFFF] ">
-      <div className="container mx-auto w-[85%] gap-10 sm:gap-8 md:gap-12  flex sm:flex-row flex-col items-center">
+    <section className=" bg-[#FFFF] relative">
+      <div className="container mx-auto w-[85%] mt-4 md:mt-0 md:gap-12 flex sm:flex-row flex-col items-center">
         <div className='flex-1 px-9 sm:px-6 md:px-9 pt-10'>
           <h1 className="font-georgia text-primary text-3xl font-bold">Transform Your Team!</h1>
-          <p className="font-georgia text-2xl mt-8 mb-4">Drive employee growth, foster innovation, and enhance your company&apos;s performance.</p>
+          <p className="font-georgia text-2xl mt-8 mb-20">Drive employee growth, foster innovation, and enhance your company&apos;s performance.</p>
           <img src={bg} alt="" />
         </div>
-        <div className='w-full sm:flex-1 mb-4 sm:mb-0 border-2 border-[#0000ff] rounded-3xl px-10 sm:px-7 md:px-12 py-8 flex flex-col gap-5'>
+        <div style={{ boxShadow: '5px 10px 20px 0px rgba(0, 0, 0, 0.25)' }} className='w-full sm:flex-1 my-8 bg-[#EAF6FC] rounded-3xl px-10 sm:px-7 md:px-12 py-8 flex flex-col gap-5'>
           <h2 className="text-3xl text-primary font-lobster text-center">Career Leap</h2>
-          <h4 className='font-plus-jakarta-sans text-3xl font-semibold text-center'>Hello! Welcome</h4>
-          <form action="" className='flex flex-col gap-4' onSubmit={handleSubmit(onsubmit)}>
+          <h4 className='font-plus-jakarta-sans text-3xl font-semibold text-center'>Create an Account</h4>
+          <p className='text-center text-[#000]'>Sign up on Career Leap as a company and get access to the services we offer.</p>
+          <form action="" className='flex flex-col gap-5' onSubmit={handleSubmit(onsubmit)}>
             <div>
-              <label className='pb-2.5 font-medium text-xl font-plus-jakarta-sans  text-primary' htmlFor='email'>Email</label>
+              <label className='font-medium text-xl font-plus-jakarta-sans  text-[#000]' htmlFor='email'>Email</label>
               <div className='relative'>
                 <input
                   name='email'
                   type='email'
                   placeholder='example@gmail.com'
-                  className={`w-full border ${errors.email ? 'border-red-500' : 'border-[#0A0A29]'} rounded-md outline-none p-2.5 leading-none font-medium text-lg placeholder:text-[#C8C8DC]`}
+                  className={`w-full border ${errors.email ? 'border-red-500' : 'border-[#0A0A29]'} mt-2 bg-transparent rounded-md outline-none p-2 leading-none font-medium text-lg placeholder:text-[#C8C8DC]`}
                   {...register('email')}
                 />
                 <AiOutlineMail className='absolute text-lg right-4 top-3.5' />
@@ -52,13 +54,13 @@ const RegisterAdmin = () => {
             </div>
 
             <div>
-              <label className='pb-2.5 font-medium text-xl font-plus-jakarta-sans  text-primary' htmlFor='password'>Password</label>
+              <label className='font-medium text-xl font-plus-jakarta-sans  text-[#000]' htmlFor='password'>Password</label>
               <div className='relative'>
                 <input
                   name='password'
                   type={showPassword ? 'text' : 'password'}
                   placeholder='***********'
-                  className={`w-full border ${errors.password ? 'border-red-500' : 'border-[#0A0A29]'} rounded-md outline-none p-2.5 leading-none font-medium text-lg placeholder:text-[#C8C8DC]`}
+                  className={`w-full border ${errors.password ? 'border-red-500' : 'border-[#0A0A29]'} mt-2 bg-transparent rounded-md outline-none p-2 leading-none font-medium text-lg placeholder:text-[#C8C8DC]`}
                   {...register('password')}
                 />
                 {
@@ -70,23 +72,23 @@ const RegisterAdmin = () => {
             </div>
 
             <div>
-              <label className='pb-2.5 font-medium text-xl font-plus-jakarta-sans  text-primary' htmlFor='organization'>Organization name</label>
+              <label className='font-medium text-xl font-plus-jakarta-sans  text-[#000]' htmlFor='organization'>Organization name</label>
               <input
                 name='organization'
                 type='text'
                 placeholder='Your organization plc...'
-                className={`w-full border ${errors.organization ? 'border-red-500' : 'border-[#0A0A29]'} rounded-md outline-none p-2.5 leading-none font-medium text-lg placeholder:text-[#C8C8DC]`}
+                className={`w-full border ${errors.organization ? 'border-red-500' : 'border-[#0A0A29]'} mt-2 bg-transparent rounded-md outline-none p-2 leading-none font-medium text-lg placeholder:text-[#C8C8DC]`}
                 {...register('organization')}
               />
               <p className='text-red-500'>{errors.organization?.message}</p>
             </div>
 
             <div>
-              <label className='pb-2.5 font-medium text-xl font-plus-jakarta-sans  text-primary' htmlFor='role'>Register as</label>
+              <label className='font-medium text-xl font-plus-jakarta-sans text-[#000]' htmlFor='role'>Register as</label>
               <select
                 name='role'
                 placeholder='Manager'
-                className={`w-full border ${errors.role ? 'border-red-500' : 'border-[#0A0A29]'} bg-white rounded-md outline-none p-2.5 leading-none font-medium text-lg placeholder:text-[#C8C8DC]`}
+                className={`w-full border ${errors.role ? 'border-red-500' : 'border-[#0A0A29]'} mt-2 bg-transparent rounded-md outline-none p-2 leading-none font-medium text-lg placeholder:text-[#C8C8DC]`}
                 {...register('role')}
               >
                 <option value="admin">HR/CEO</option>
@@ -96,11 +98,15 @@ const RegisterAdmin = () => {
             </div>
 
             <button type='submmit' onClick={handleSubmit(onsubmit)} className='bg-[#3333FF] text-[#F0F0FF] rounded-lg px-5 py-2.5 text-2xl font-semibold'>Sign In</button>
-            <p className='text-xl font-plus-jakarta-sans font-semibold text-center'>Already have an account?
-              <Link className='text-primary underline' href="/login">Log in</Link>
+            <p className='text-xl font-plus-jakarta-sans font-semibold text-center'>Have an account?
+              <Link className='text-primary pl-2' href="/login">Log in</Link>
             </p>
           </form>
         </div>
+        <Link to='/' className='absolute top-0 md:top-6 left-[8%] flex items-center gap-3 text-xl text-[#000] font-semibold'>
+          <BsArrowLeft />
+          Go Back
+        </Link>
       </div>
 
     </section>
