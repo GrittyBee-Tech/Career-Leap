5a87eeffe74cbcc1822e7b14729301ce5dbbdd44
fiiwebarry <fiiwebarry@yahoo.com>
2023-11-10 20:01:27 +0100

    dashboard

---
+++ src/Components/Dashboard-Layout/Navbar.jsx
@@ -0,0 +1,14 @@
+
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h2 className="">Rise Path</h2>
+
+            <input type="text" placeholder="search" />
+
+        </nav>
+    )
+}
+
+export default NavBar
---
+++ src/Components/Dashboard-Layout/OverView.jsx
@@ -0,0 +1,12 @@
+
+
+const OverView = () => {
+    return (
+        <section>
+
+
+        </section>
+    )
+}
+
+export default OverView
