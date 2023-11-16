4e42ccce35a61e27fb43b71347c8fc16e8bca8a6
fiiwebarry <fiiwebarry@yahoo.com>
2023-11-16 15:55:16 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
+import SideBarContext from './ContextApi/SideBarContext.jsx';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SideBarContext>
+        <App />
+      </SideBarContext>
+
     </BrowserRouter>
   </React.StrictMode>,
 )
