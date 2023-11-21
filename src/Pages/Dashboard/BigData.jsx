165c51ff4742efa9568f3010f5449932f7dae3a3
fiiwebarry <fiiwebarry@yahoo.com>
2023-11-22 00:15:42 +0100

    update

---
+++ src/Pages/Dashboard/BigData.jsx
@@ -0,0 +1,69 @@
+/* eslint-disable react-refresh/only-export-components */
+import mentor from "/src/assets/Images/Tedd.png"
+import think from "/src/assets/Images/E-books/think-for-yourself-control.jpg"
+import icon from "/src/assets/Images/E-books/strategy.jpg"
+import pdf from "/src/assets/pdf/think.pdf"
+
+export const Ebooks = [
+    { id: 1, title: "It’s Not You, It’s Your Strategy",  Image:icon, content: "We tend to punish ourselves harshly for our “failure,” feeling lots of shame and guilt, and sometimes others – even family and friends – punish us as well.",pdf:doc},
+     { id: 5, title: "Think of Yourself Control Your life",  Image: think, content: "There are many aspects to following a successful career. They are mostly derived from two characteristics of personality: ability and application.", },
+    
+];
+
+
+export const mentorVideos = [
+    {
+        id: 1,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+    {
+        id: 2,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+    {
+        id: 3,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+    {
+        id: 4,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+    {
+        id: 5,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+    {
+        id: 6,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+    {
+        id: 7,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+    {
+        id: 8,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+    {
+        id: 9,
+        title: "The Power of Entrepreneurial Mindset",
+        video: mentor,
+        author: "Bill Roche",
+    },
+];
