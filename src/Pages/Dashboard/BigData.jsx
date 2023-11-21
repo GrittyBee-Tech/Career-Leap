dc2b04ba6e2a24607bfd1fc467b27520b65d63b8
fiiwebarry <fiiwebarry@yahoo.com>
2023-11-21 12:33:09 +0100

    update

--- src/Pages/Dashboard/BigData.jsx
+++ src/Pages/Dashboard/BigData.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable react-refresh/only-export-components */
 import mentor from "/src/assets/Images/Tedd.png"
+import icon from "/src/assets/Images/E-books/strategy.jpg"
+import icon2 from "/src/assets/Images/E-books/tolerance-harmony-in-difference-ef1f0a18e452e8f26c9a86ad2b781c66.jpg"
 
 export const Ebooks = [
 
-    { id: 1, title: "CPR AED & First AID", content: "CPR, AED, and First Aid Provider Handbook is based on the 2020-2025 CPR, AED, and First Aid guidelines published by the International Liaison Committee on Resuscitation and is designed to train people everywhere to respond to everyday emergencies" },
-    { id: 2, title: "A Conversation about Healthy Eating", content: "A Conversation about Healthy Eating brings together all the relevant science about healthy eating in one place, and it’s exactly that – a conversation; an informal discussion between a scientist and a friend about their eating habits, keeping the science firmly rooted in everyday life", },
-    { id: 3, title: "Bad Breath to Sweet Breath", content: "Though the reasons for bad breath odour are not completely understood, most unpleasant odours are known to occur due to food debris trapped in the mouth. It is really amazing to find as many as 400 different types of bacteria in an average mouth! Trouble can occur when several dozen of these are allowed to flourish in large proportions or are genetically mutated to reproduce in a large number", },
-    { id: 4, title: "How to Succeed in Your Work: a tool kit", content: "There are many aspects to following a successful career. They are mostly derived from two characteristics of personality: ability and application. A person with much ability has an easier time than another who is less able but the individual with less ability can also succeed through greater application.", },
-    { id: 5, title: "Better Ways to Manage Meetings", content: "Meetings are a normal, everyday part of humanitarian life. Many of us have experienced coming away from a meeting with a sense of accomplishment and satisfaction, without necessarily knowing why the meeting was successful" },
-    { id: 6, title: "It’s Not You, It’s Your Strategy", content: "We tend to punish ourselves harshly for our “failure,” feeling lots of shame and guilt, and sometimes others – even family and friends – punish us as well.", },
+    { id: 1, title: "How to Succeed in Your Work: a tool kit", Image: icon2, content: "There are many aspects to following a successful career. They are mostly derived from two characteristics of personality: ability and application. A person with much ability has an easier time than another who is less able but the individual with less ability can also succeed through greater application.", },
+    { id: 2, title: "It’s Not You, It’s Your Strategy", Image: icon, content: "We tend to punish ourselves harshly for our “failure,” feeling lots of shame and guilt, and sometimes others – even family and friends – punish us as well.", },
 ];
 
 
